12d33d2bf06b03cf72d493a6540bdb7f94481a07
Vipin4147 <vipin4147@gmail.com>
2023-06-19 13:00:06 +0530

    fixing bar

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -4,8 +4,8 @@ let sarr = [
   "https://www.lakeshoresf.com/wp-content/uploads/2021/04/Kids-Fitness-Training-Lincoln-Park-Chicago-scaled.jpeg",
 ];
 
-let name = localStorage.getItem('userName');
-if(name){
+let name = localStorage.getItem("userName");
+if (name) {
   let n = document.querySelector("#user>span");
   n.innerHTML = name;
 }
@@ -114,3 +114,55 @@ let user = document.getElementById("user");
 user.onclick = () => {
   window.location.href = "./pages/login.html";
 };
+
+const city = document.querySelector("#location>span");
+
+getLocation();
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        var geocodingAPI =
+          "https://api.openweathermap.org/geo/1.0/reverse?lat=" +
+          latitude +
+          "&lon=" +
+          longitude +
+          "&limit=1&appid=8b908a8f4e3018596a0c6ea121b4bd30";
+
+        fetch(geocodingAPI)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            city.innerHTML = data[0].name.toUpperCase();
+
+            console.log(city.innerText);
+          })
+          .catch(function (error) {
+            console.log("Error:", error);
+          });
+      },
+      function (error) {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.");
+            break;
+          case error.TIMEOUT:
+            console.log("The request to get user location timed out.");
+            break;
+          case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.");
+            break;
+        }
+      }
+    );
+  } else {
+    console.log("Geolocation is not supported.");
+  }
+}
--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,21 +1,70 @@
-
-let user = JSON.parse(localStorage.getItem("user"))
+let user = JSON.parse(localStorage.getItem("user"));
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const form = document.querySelector("form");
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    if(!user){
-        window.location.href = "../pages/signup.html"
-    }
-    else
-    if(user.password === password.value && user.email === email.value){
-        localStorage.setItem("userName",user.name)
-        
-        window.location.href = "../index.html"
-    }else{
-        alert("Please enter correct password")
-    }
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!user) {
+    window.location.href = "../pages/signup.html";
+  } else if (user.password === password.value && user.email === email.value) {
+    localStorage.setItem("userName", user.name);
+
+    window.location.href = "../index.html";
+  } else {
+    alert("Please enter correct password");
+  }
+});
+
+const city = document.querySelector("#location>span");
+
+getLocation();
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        var geocodingAPI =
+          "https://api.openweathermap.org/geo/1.0/reverse?lat=" +
+          latitude +
+          "&lon=" +
+          longitude +
+          "&limit=1&appid=8b908a8f4e3018596a0c6ea121b4bd30";
+
+        fetch(geocodingAPI)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            city.innerHTML = data[0].name.toUpperCase();
+
+            console.log(city.innerText);
+          })
+          .catch(function (error) {
+            console.log("Error:", error);
+          });
+      },
+      function (error) {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.");
+            break;
+          case error.TIMEOUT:
+            console.log("The request to get user location timed out.");
+            break;
+          case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.");
+            break;
+        }
+      }
+    );
+  } else {
+    console.log("Geolocation is not supported.");
+  }
+}
